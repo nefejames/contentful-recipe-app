b3e3e3f8e9e10a3110e6c50a983856bcfc6fe3fd
Emadamerho-Atori Nefe <nefejames@yahoo.com>
2021-07-26 15:44:43 -0700

    switched to SC; changed copy

--- pages/404.js
+++ pages/404.js
@@ -1,35 +1,46 @@
-import Link from 'next/link'
-import { useEffect } from 'react'
-import { useRouter } from 'next/router'
+import Link from "next/link";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import styled from "styled-components";
 
 const NotFound = () => {
-  const router = useRouter()
+  const router = useRouter();
 
   useEffect(() => {
     setTimeout(() => {
-      router.push('/')
-    }, 4000)
-  }, [])
+      router.push("/");
+    }, 4000);
+  }, []);
 
   return (
-    <div className="not-found">
+    <Container>
       <h1>404</h1>
       <h2>Oops! That page cannot be found :(</h2>
-      <p>Redirecting to <Link href="/"><a>Homepage</a></Link> for more marmite goodness...</p>
-
-      <style jsx>{`
-        .not-found {
-          background: #fff;
-          padding: 30px;
-          box-shadow: 1px 3px 5px rgba(0,0,0,0.1);
-          transform: rotateZ(-1deg);
-        }
-        h1 {
-          font-size: 3em;
-        }
-      `}</style>
-    </div>
+      <p>
+        Redirecting to{" "}
+        <Link href="/">
+          <a>Homepage</a>
+        </Link>{" "}
+      </p>
+    </Container>
   );
-}
- 
+};
+
 export default NotFound;
+
+const Container = styled.div`
+  background: #fff;
+  padding: 30px;
+  box-shadow: 1px 3px 5px rgba(0, 0, 0, 0.1);
+  transform: rotateZ(-1deg);
+
+  h1 {
+    font-size: 3em;
+    margin-bottom: 1rem;
+  }
+
+  h2 {
+    font-size: 1.4rem;
+    margin-bottom: 1rem;
+  }
+`;
