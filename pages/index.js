991d59435a9f54f9aab49bcef4752b4cd1973507
Emadamerho-Atori Nefe <nefejames@yahoo.com>
2021-07-26 15:39:05 -0700

    switched to sc

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
 import { createClient } from "contentful";
 import RecipeCard from "../components/RecipeCard";
 import Head from "next/head";
+import styled from "styled-components";
 export default function Recipes({ recipes }) {
   return (
-    <>
+    <Container>
       <Head>
         <title>Contentful Recipe App</title>
       </Head>
@@ -11,18 +12,22 @@ export default function Recipes({ recipes }) {
         {recipes.map((recipe) => (
           <RecipeCard key={recipe.sys.id} recipe={recipe} />
         ))}
-
-        <style jsx>{`
-          .recipe-list {
-            display: grid;
-            grid-template-columns: 1fr 1fr;
-            grid-gap: 20px 60px;
-          }
-        `}</style>
       </div>
-    </>
+    </Container>
   );
 }
+
+const Container = styled.div`
+  .recipe-list {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-gap: 20px 60px;
+
+    @media (max-width: 768px) {
+      grid-template-columns: 1fr;
+    }
+  }
+`;
 
 export async function getStaticProps() {
   const client = createClient({
